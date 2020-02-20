d3eeab788e5360513d60cc50207172f0184eee65
Spelaia58 <Spelaia58@gmail.com>
2020-02-20 16:08:32 +0200

    w5

---
+++ task-3.js
@@ -0,0 +1,40 @@
+class Storage {
+    constructor(items = []){
+        this.items = items;
+    }
+    getItems(){
+        return this.items;  
+    }
+
+   addItem(item){
+    items.push(item);
+   }
+   removeItem(item){
+       
+       for (let i = 0; i < items.length; i ++) {
+           if(items[i] == item){
+              items.splice(i, 1);
+           }
+       }
+               
+           
+       }
+   }
+
+
+
+const storage = new Storage([
+    'Нанитоиды',
+    'Пролонгер',
+    'Железные жупи',
+    'Антигравитатор',
+  ]);
+  
+  const items = storage.getItems();
+  console.table(items);
+
+  storage.addItem('Дроид');
+  console.table(storage.items); 
+  
+  storage.removeItem('Пролонгер');
+  console.table(storage.items); 
---
+++ task-5.js
@@ -0,0 +1,59 @@
+class Car {
+  static getSpecs(Car) {
+    console.log(
+        `Max Speed:${Car.maxSpeed}, Speed:${Car.speed}, Is On:${Car.isOn}, Distance:${Car.distance}, Price:${Car.price} `
+    );
+  }
+  constructor({maxSpeed, price}) {
+    this.maxSpeed = maxSpeed;
+    this._price = price;
+    this.speed = 0;
+    this.isOn = false;
+    this.distance = 0;
+  }
+  get price() {
+    return this._price;
+  }
+  set price(newPrice) {
+    this._price = newPrice;
+  }
+
+  turnOn() {
+    this.isOn = true;
+  }
+  turnOff() {
+    this.isOn = false;
+    this.speed = 0;
+  }
+  accelerate(value) {
+    if (this.maxSpeed >= this.speed + value) {
+      return (this.speed += value);
+    }
+  }
+  decelerate(value) {
+    if (this.speed - value >= 0) {
+      return (this.speed -= value);
+    }
+  }
+  drive(hours) {
+    this.distance = hours * this.speed * this.distance;
+  }
+}
+
+const mustang = new Car({ maxSpeed: 200, price: 2000 });
+
+mustang.turnOn();
+mustang.accelerate(50);
+mustang.drive(2);
+
+Car.getSpecs(mustang);
+
+mustang.decelerate(20);
+mustang.drive(1);
+mustang.turnOff();
+
+Car.getSpecs(mustang);
+
+console.log(mustang.price);
+mustang.price = 4000;
+console.log(mustang.price);
